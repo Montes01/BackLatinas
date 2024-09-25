be0f933ea35cc4747f7c8331f3966d7c8f131ecd
Montes01 <samuel.montes@pragma.com.co>
2024-09-25 01:13:34 -0500

    Refactor routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import './App.css'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { routes } from './lib/constants/routes'
 import { Home } from './components/organisms/home'
 function App() {
 
   return (
     <BrowserRouter>
-      <Route path={routes.HOME.BASE}> <h1>Home</h1> </Route>
-      <Route path={routes.HOME.HOME}>
-        <Home />
-      </Route>
+      <Routes>
+        <Route path={routes.HOME.HOME} element={<Home />} />
+      </Routes>
     </BrowserRouter>
   )
 }
