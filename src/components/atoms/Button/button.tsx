a3c5b93c36323bc5cd987c3b3e12ff67c232434b
Montes01 <samuel.montes@pragma.com.co>
2024-10-01 08:10:32 -0500

    Refactor Button component to make disabled and url props optional

--- src/components/atoms/Button/button.tsx
+++ src/components/atoms/Button/button.tsx
@@ -3,11 +3,11 @@ import styles from './button.module.scss';
 interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
     text: string;
     onClick?: () => void;
-    type: "button" | "submit" | "reset";
-    disabled: boolean;
+    type?: "button" | "submit" | "reset";
+    disabled?: boolean;
     url?: string;
 }
-export const Button = ({ text, onClick, type = 'button', disabled, url, ...rest }: ButtonProps) => {
+export const Button = ({ text, onClick, type = 'button', disabled = false, url, ...rest }: ButtonProps) => {
     const handleUncoherentProps = () => {
         if (url && type !== 'button') {
             throw('You can only pass a url prop to a button with type button');
