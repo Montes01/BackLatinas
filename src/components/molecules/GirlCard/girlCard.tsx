c1963ba7d9b887a1ec73aa1f622e94fbd5d1ad5f
Montes01 <samuel.montes@pragma.com.co>
2024-10-12 01:34:07 -0500

    fix some errors girlCard

--- src/components/molecules/GirlCard/girlCard.tsx
+++ src/components/molecules/GirlCard/girlCard.tsx
@@ -3,17 +3,17 @@ import { Women } from '../../../lib/types/types'
 import { Button } from '../../atoms/Button/button'
 import { StarRate } from '../../atoms/StarRate/StarRate'
 import styles from './girlCard.module.scss'
-export const GirlCard = ({ user: { id_user, user_name, profile_photo, nacionality } }: Women) => {
+export const GirlCard = ({ user: { id_user, profile_photo, nacionality }, name }: Women) => {
 
     const onImageError = (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
         event.currentTarget.src = '/assets/default-girl.png'
     }
     return (
         <article className={styles.girlCard}>
-            <img onError={onImageError} className={styles.girlCard__picture} src={profile_photo} alt={user_name} />
+            <img onError={onImageError} className={styles.girlCard__picture} src={profile_photo} alt={name} />
             <div className={styles.girlCard__info}>
                 <div className={styles.girlCard__info__nameContainer}>
-                    <h3 className={styles.girlCard__name}>{user_name}</h3>
+                    <h3 className={styles.girlCard__name}>{name}</h3>
                     {/* {isVerified && <StarRate />} */}
                 </div>
                 <p className={styles.girlCard__country}>{nacionality}</p>
