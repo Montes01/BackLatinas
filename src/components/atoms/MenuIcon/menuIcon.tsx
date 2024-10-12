e6b42f558842acd083f99a1de41a61bda2df1e6d
Montes01 <samuel.montes@pragma.com.co>
2024-10-12 02:38:03 -0500

    Refactor girl module styles and add language selection menu

---
+++ src/components/atoms/MenuIcon/menuIcon.tsx
@@ -0,0 +1,21 @@
+//button props
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    onClick: () => void
+}
+
+import styles from './menuIcon.module.scss';
+
+export const IconMenu = ({ onClick, ...props }: Props) => {
+    return (
+        <button onClick={onClick} className={styles.menuIcon} {...props}>
+            <svg
+                viewBox="0 0 1024 1024"
+                fill="currentColor"
+                height="2em"
+                width="2em"
+            >
+                <path d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z" />
+            </svg>
+        </button>
+    );
+}
--- src/components/molecules/header/header.tsx
+++ src/components/molecules/header/header.tsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
 import { HOME_TEXTS } from "../../../lib/constants/homeConstants";
+import { IconMenu } from "../../atoms/MenuIcon/menuIcon";
 import { NavButton } from "../../atoms/NavButton/navButton"
 import styles from './header.module.scss';
+import ReactFlagsSelect from "react-flags-select";
 export const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [selectedCountry, setSelectedCountry] = useState('US');
+    const handleMenuClick = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
     return (
         <header className={styles.header}>
             <header className={styles.header__nav}>
-                <NavButton text="Home" path="/home" />
-                <NavButton text="Girls" path="/home/girls" />
-                <NavButton text="Comments" path="/comments" />
-                <NavButton text="Members" path="/members" />
-                <NavButton text="Login" path="/login" />
+                <div className={styles.header__nav__menu}>
+                    <ReactFlagsSelect
+                        showSelectedLabel={false}
+                        showOptionLabel={false}
+                        className={styles.header__nav__menu__language}
+                        selected={selectedCountry}
+                        onSelect={(el) => setSelectedCountry(el)} countries={["US", "GB", "FR", "DE", "IT", "ES"]}
+                    />
+                    <IconMenu onClick={handleMenuClick} />
+                    <div className={styles[`header__nav__menu__items${isMenuOpen ? '' : '--closed'}`]}>
+                        <NavButton text="Sign in" path="/sign-in" className={styles.header__nav__menu__items__main} />
+                        <NavButton text="Sign up" path="/sign-up" className={styles.header__nav__menu__items__main} />
+                        <hr />
+                        <NavButton text="Home" path="/home" />
+                        <NavButton text="Girls" path="/home/girls" />
+                        <NavButton text="Comments" path="/comments" />
+                        <NavButton text="Join our team" path="/join" />
+                    </div>
+                </div>
+
             </header>
             <main className={styles.header__content}>
                 <h1 className={styles.header__content__title}> {HOME_TEXTS.TITLE}</h1>
