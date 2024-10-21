a6e91a03474113bab655bfece3865266fd10ef28
Montes01 <samuel.montes@pragma.com.co>
2024-10-21 07:29:13 -0500

    Refactor Login component to add error handling and modal for login status

--- src/components/templates/login/index.tsx
+++ src/components/templates/login/index.tsx
@@ -10,10 +10,17 @@ import { useAppDispatch } from "../../../lib/contexts/hooks";
 import { login } from "../../../lib/services/api";
 import { LoginResponse } from "../../../lib/types/types";
 import { setUser } from "../../../lib/contexts/auth/authSlice";
+import { AlertModal, AlertModalProps } from "../../molecules/AlertModal/alertModal";
 
 export const Login = () => {
     const [error, setError] = useState("");
     const [isLoading, setIsLoading] = useState(false);
+    const [modalProps, setModalProps] = useState({
+        isOpen: false,
+        isLoading: false,
+        message: '',
+        onOk: () => { }
+    } as AlertModalProps);
     const dispatch = useAppDispatch();
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
@@ -27,15 +34,31 @@ export const Login = () => {
             setIsLoading(false);
             return;
         }
+        setModalProps(props => ({
+            ...props, isLoading: true, isOpen: true, message: 'Logging in', onOk: () => {
+                setModalProps(props => ({ ...props, isOpen: false }));
+            }
+        }));
         const body = { email: email, password: password }
         const callback = (user: LoginResponse) => {
             dispatch(setUser(user));
         }
 
         try {
-           await login(body, callback);
+            await login(body, callback);
+            setModalProps(props => ({
+                ...props, isLoading: false, isOpen: true, message: 'Logged in', onOk: () => {
+                    setModalProps(props => ({ ...props, isOpen: false }));
+                    window.location.href = '/';
+                }
+            }))
         } catch (err) {
             setError((err as Error).message)
+            setModalProps(props => ({
+                ...props, isLoading: false, isOpen: true, message: 'incorrect email or password', onOk: () => {
+                    setModalProps(props => ({ ...props, isOpen: false }));
+                }
+            }))
         }
         finally {
             setIsLoading(false);
@@ -43,26 +66,29 @@ export const Login = () => {
     };
 
     return (
-        <div className={styles.loginPage}>
-            <Header />
-            <main className={styles.large_section_wrapper}>
-                <BackButton />
-                <div className={styles.loginContainer}>
-                    <h2 className={styles.title}>Sign In</h2>
-                    {error && <div className={styles.errorMessage}>{error}</div>}
-                    <form onSubmit={handleSubmit} className={styles.form}>
-                        <Input label="E-mail" name="email" type="email" placeholder="Enter your user e-mail" required />
-                        <Input label="Password" name="password" type="password" placeholder="Enter your password" required />
-                        <Link href="/forgot-password" className={`${styles.forgotPassword} ${styles.yellowUnderline}`}>
-                            Forgot your password?
-                        </Link>
-                        <button type="submit" className={styles.submitButton} disabled={isLoading}>
-                            {isLoading ? 'Cargando...' : 'Confirmar'}
-                        </button>
-                    </form>
-                </div>
-            <Footer />
-            </main>
-        </div>
+        <>
+            <div className={styles.loginPage}>
+                <Header />
+                <main className={styles.large_section_wrapper}>
+                    <BackButton />
+                    <div className={styles.loginContainer}>
+                        <h2 className={styles.title}>Sign In</h2>
+                        {error && <div className={styles.errorMessage}>{error}</div>}
+                        <form onSubmit={handleSubmit} className={styles.form}>
+                            <Input label="E-mail" name="email" type="email" placeholder="Enter your user e-mail" required />
+                            <Input label="Password" name="password" type="password" placeholder="Enter your password" required />
+                            <Link href="/forgot-password" className={`${styles.forgotPassword} ${styles.yellowUnderline}`}>
+                                Forgot your password?
+                            </Link>
+                            <button type="submit" className={styles.submitButton} disabled={isLoading}>
+                                {isLoading ? 'Cargando...' : 'Confirmar'}
+                            </button>
+                        </form>
+                    </div>
+                    <Footer />
+                </main>
+            </div>
+            <AlertModal {...modalProps} />
+        </>
     );
 }
