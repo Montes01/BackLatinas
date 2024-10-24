69c8481dfff0d63b6f8a77dec960bd39affa5c9a
Montes01 <samuel.montes@pragma.com.co>
2024-10-24 16:08:46 -0500

    Refactor Button component to use NavLink instead of anchor tag

--- src/components/atoms/Button/button.tsx
+++ src/components/atoms/Button/button.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import styles from './button.module.scss';
+import { NavLink } from "react-router-dom";
 interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
     text: string;
     onClick?: () => void;
@@ -19,8 +20,8 @@ export const Button = ({ text, onClick, type = 'button', disabled = false, url, 
     return !url ? (
         <button {...rest} className={`${styles.button} ${rest.className}`} type={type} onClick={onClick} disabled={disabled}>{text}</button>
     ) : (
-        <a className={`${styles.button} ${rest.className}`} href={url} onClick={onClick} >
+        <NavLink className={`${styles.button} ${rest.className}`} to={url} onClick={onClick} >
             {text}
-        </a>
+        </NavLink>
     )
 }
