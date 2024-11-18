ce04801bd10887506c3a054d04cb372c01cce245
Montes01 <samuel.montes@pragma.com.co>
2024-11-18 15:30:18 -0500

    Enhance SubServiceCard component: include quantity in time display for improved information clarity

--- src/components/molecules/SubServiceCard/SubServiceCard.tsx
+++ src/components/molecules/SubServiceCard/SubServiceCard.tsx
@@ -4,11 +4,11 @@ interface Props extends SubService { }
 
 import styles from './subServiceCard.module.scss'
 
-export const SubServiceCard = ({ description, price, time }: Props) => {
+export const SubServiceCard = ({ description, price, time, quantity }: Props) => {
     return (
         <div className={styles.subServiceCard}>
             <div className={styles.subServiceCard__info}>
-                <h2 className={styles.subServiceCard__info__time}> {time}</h2>
+                <h2 className={styles.subServiceCard__info__time}> {time ?? quantity}</h2>
                 <h3 className={styles.subServiceCard__info__desc}>{description}</h3>
                 <strong className={styles.subServiceCard__info__price}>{price} NOK</strong>
             </div>
